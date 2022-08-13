57e1e34ec406faa55489a8afec0463bfa19218ac
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2022-08-13 19:55:19 +0530

    Update Array Description and Meetin the middle

--- CSES/DP/Array_Description.cpp
+++ CSES/DP/Array_Description.cpp
@@ -1,101 +1,40 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int mod = 1e9+7;
-void solve(vector<int> &v,int m,int n,int &cnt)
-{
-    if(n==0)
-    {
-        bool flag = true;
-        for(int k:v)if(k==0)flag = false;
-        if(flag)
-        {
-            cnt++;
-            cnt = cnt%mod;
-        }
-        return;
+int main() {
+  int mod = 1e9+7;
+  int n, m;
+  cin >> n >> m;
+  vector<vector<int>> dp(n,vector<int>(m+1,0));
+  int x0;
+  cin >> x0;
+  if (x0 == 0) {
+    fill(dp[0].begin(), dp[0].end(), 1);
+  } else {
+    dp[0][x0] = 1;
+  }
+  for (int i = 1; i < n; i++) {
+    int x;
+    cin >> x;
+    if (x == 0) {
+      for (int j = 1; j <= m; j++) {
+	for (int k : {j-1,j,j+1}) {
+	  if (k >= 1 && k <= m) {
+	    (dp[i][j] += dp[i-1][k]) %= mod;
+	  }
+	}
+      }
+    } else {
+      for (int k : {x-1,x,x+1}) {
+	if (k >= 1 && k <= m) {
+	  (dp[i][x] += dp[i-1][k]) %= mod;
+	}
+      }
     }
-
-    if(v[n-1]==0)
-    {
-        int li;
-        if(n>=2)li = v[n-2];
-        else li = v[0];
-        int ri;
-        if(n<v.size())ri = v[n];
-        else ri = v[v.size()-1];
-        if(ri!=0){
-        for(int i=ri-1;i<=ri+1;i++)
-        {
-            if(i>=1 && i<=m)
-            {
-               if(li!=0)
-               {
-                if(i>=li-1 && i<=li+1)
-                {
-                    v[n-1]=i;
-                    solve(v,m,n-1,cnt);
-                    v[n-1]=0;
-                }
-               }else
-               {
-                v[n-1]=i;
-                solve(v,m,n-1,cnt);
-                v[n-1] = 0;
-               }
-            }
-        }
-        }else
-        {
-            for(int i=1;i<=m;i++)
-            {
-                if(li!=0)
-               {
-                if(i>=li-1 && i<=li+1)
-                {
-                    v[n-1]=i;
-                    solve(v,m,n-1,cnt);
-                    v[n-1]=0;
-                }
-               }else
-               {
-                v[n-1]=i;
-                solve(v,m,n-1,cnt);
-                v[n-1] = 0;
-               }
-            }
-        }
-    }else
-    {
-        solve(v,m,n-1,cnt);
-    }
-
-    return;
-}
-int main()
-{
-    int n,m;
-    cin>>n>>m;
-    vector<int> v;
-    for(int i=0;i<n;i++)
-    {
-        int k;
-        cin>>k;
-        v.push_back(k);
-    }
-
-    int cnt = 0;
-
-    // solve(v,m,n,cnt);
-
-    vector<int> dp(n+1,0);
-
-    for(int i=1;i<=n;i++)
-    {
-        
-    }
-
-    cnt = cnt%mod;
-    cout<<cnt;
-    return 0;
+  }
+  int ans = 0;
+  for (int j = 1; j <= m; j++) {
+    (ans += dp[n-1][j]) %= mod;
+  }
+  cout << ans << endl;
 }
