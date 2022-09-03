3632a2f78f93312717af53dfe06c4dde8b95b723
Shivam-Shandilya1 <shivamshandilya12@gmail.com>
2022-09-03 22:51:41 +0530

    Codeforce div 2

--- Codeforces/Rnd 818(Div2)/Q1.cpp
+++ Codeforces/Rnd 818(Div2)/Q1.cpp
@@ -6,6 +6,11 @@ int main()
     cin>>T;
     for(int t = 0;t<T;t++)
     {
+        int n;
+        cin>>n;
+        vector<int> v1(n),v2(n);
+        for(int i=0;i<n;i++)cin>>v1[i];
+        for(int i=0;i<n;i++)cin>>v2[i];
         
     }
     return 0;
